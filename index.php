7c3e97cb42d4bdba6e774c88d8dcbf8f68f10c7c
mayumi charaña <mayumi-24@cls>
2016-03-18 11:43:48 -0500

    examen

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+	<link rel="stylesheet" href="public/css/estilos.css">
+</head>
+<body>
+<br>
+
+	<div class="contenedor">
+        <nav>
+          <ul class="menu-lista">
+		     <li class="menu-item"><a href="inicio.html" class="menu-link">INICIO</a></li>
+		     <li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+		     <li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+		     <li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+		     <li class="menu-item"><a href="" class="menu-link">CONTÁCTENOS</a></li>
+          <ul>
+        </nav>
+    <div>
+	
+<br>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+	<link rel="stylesheet" href="public/css/estilos.css">
+
+</head>
+<body>
+<div class="container">
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">TIENDA DE MUEBLES</a>
+			</div>
+	
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Link</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li><a href="#">Separated link</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+</div>
+
+
+<div id="carousel-id" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="item">
+			<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/slider/mode1.jpg">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Example headline.</h1>
+					<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item">
+	 			<img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="public/slider/mode2.jpg">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Another example headline.</h1>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item active">
+			<img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="public/slider/mode6.jpg">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>One more for good measure.</h1>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+
+
+
+
+	<<script src="public/js/jquery-1.12.1.js"></script>
+	<<script src="public/js/bootstrap.js"></script>
+
+<h2>En muchos casos cuando nos planteamos la decoración de un dormitorio la cabecera de la cama no es un elemento clave. Aunque debemos recalcar que cuando se trata de camas diseño o decoración la cabecera es también importante. Para que la cabecera tenga el impacto necesario en la decoración en general, es ineludible que se destaquen de alguna manera. </h2>
+
+
+</body>
+</html>
+
+		
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+		<img src="public/slider/exa4.jpe" class="img-circle"></div>
+		<div class="col-md-4"><img src="public/slider/exa5.jpe" class="img-circle" alt=""></div>
+		<div class="col-md-4"><img src="public/slider/exa7.jpe" class="img-circle" alt=""></div>
+		
+	</div>
+		
+
+<br>
+
+	<div class="row">
+		<div class="col-md-4"><img src="public/slider/exa6.jpe" class="img-circle"alt=""></div>
+		<div class="col-md-4"><img src="public/slider/exa8.jpe" class="img-circle" alt=""></div>
+		<div class="col-md-4"><img src="public/slider/exa6.jpe" class="img-circle" alt=""></div>
+		
+	
+<br>
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<h3 class="panel-title">Camas diseño de cabecera combinado con lámparas</h3>
+				</div>
+				<div class="panel-body">
+					<img class="img-responsive" src="public/slider/cu1.jpg" alt="">
+					<p>Esta variante te propongo hoy camas diseño y cabeceros negros que de seguro le darán carácter a cualquier habitación que tengamos en casa. Un punto focal se puede lograr con las cabeceras que incluyen las mesas de noche. Siempre debemos tratar de armonizarlo con otros accesorios en la habitación. Si agregamos algunos detalles en rojo, por ejemplo, crearemos un ambiente vibrante. Sin dudas hay muchas maneras de hacer que la cabecera de la cama sea un elemento atractivo en el dormitorio. Hay casos de combinaciones con el blanco que crean relieve y es por demás atemporal.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<h3 class="panel-title">Camas diseño y cabecera para dormitorio infantil</h3>
+				</div>
+				<div class="panel-body">
+					<img class="img-responsive" src="public/slider/cu2.jpg" alt="">
+					<p>Es una variante que aunque no sea muy compleja siempre se destacará. Lo importante es que el negro del cabecero se coordine con otras piezas de la habitación. Si buscamos elegancia el cuero es uno de esos materiales que en materia de diseño camas y cabeceras no puede ser ignorado. El impacto siempre será mayor si la cabecera se combina con otros elementos grandes como paredes de fondo oscuras. Así la cabecera se ve integrada en el espacio y no como un accesorio extra.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<h3 class="panel-title">Variante para techo inclinado</h3>
+				</div>
+				<div class="panel-body">
+					<img class="img-responsive" src="public/slider/cu3.jpg" alt="">
+					<p>Sin dudas un fondo más claro puede por otro lado enfatizar en la forma y tamaño de la cabecera. Sin dudas el negro es el color adecuado si queremos resaltar nuestra cabecera. Si buscamos destacar el ornamento no es adecuado porque podría ocultar algunos detalles. Si apostamos por la forma y el tamaño ésta es la opción. Disfruta la galería de hoy, con variantes para muchos gustos y situaciones reales en cuanto a tamaño y espacio.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+<div class="container">
+	
+	  
+	<div class="row">
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/video/muebles multifuncionales para espacios reducidos.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/video/Catalogo de camas abatibles.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/video/Muebles Juveniles JJP en la Feria del Mueble de Zaragoza 2012.mp4">
+				</video>
+			</div>
+		</div>
+		 
+	</div>
+
+	<br>
+
+<h1> my music</h1>
+
+
+<audio autobuffer autoloop loop controls>
+	<source src="public/audio/This is me- CAMP ROCK (Traducido a español).mp3">
+	<source src="/media/audio.wav">
+	<object type="audio/x-wav" data="/media/audio.wav" width="290" height="45">
+		<param name="src" value="/media/audio.wav">
+		<param name="autoplay" value="false">
+		<param name="autoStart" value="0">
+		<p><a href="/media/audio.wav">Download this audio file.</a></p>
+	</object>
+</audio>
+		
+</body>
+</html>
